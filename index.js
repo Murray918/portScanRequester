dffc27142d4e1da116c00316fe41c4fac377baa8
Andrew Murray <darbyguitars@gmail.com>
2019-05-13 15:05:29 -0500

    added express and an a route to initiate the port scan working on getting them connected and loging the host of the requests ports

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
-const writeFile = require('./wrtie-to-file')
+const express = require('express')
 
-writeFile('./tmp/test.txt', 'woot woot woot party!!!!!!!!')
+const writeFile = require('./wrtie-to-file')
+const scanPorts = require('./port-scan')
+
+const timeout = 2000
+const end = 10000
+const start = 1
+const host = 'localhost'
+const PORT = 8080
+
+let app = express()
+//test writing to a file will end up writing a test for this later
+// writeFile('./tmp/test.txt', 'woot woot woot party!!!!!!!!')
+scanPorts(host, start, end, timeout)
+
+app.get('/', (request, response) => {
+  let host = request.hostname
+  writeFile('./tmp/port-log.txt', request.hostname)
+  response.send(host)
+})
+
+app.listen(PORT, () => {
+  console.log(`Server is running on ${PORT}`)
+})
--- port-scan.js
+++ port-scan.js
@@ -1,47 +1,53 @@
 const net = require('net')
 
 // the machine to scan
-const host = 'localhost'
+// const host = 'localhost'
 // starting from port number
-let start = 1
+// let start = 1
 // to port number
-const end = 10000
+// const end = 10000
 // sockets should timeout asap to ensure no resources are wasted
-// but too low a timeout value increases the likely-hood of missing open sockets, so be careful
-const timeout = 2000
+// but too low a timeout value increases the likelyhood of missing open sockets, so be careful
+// const timeout = 2000
 
 // the port scanning loop
-while (start <= end) {
-  // it is always good to give meaningful names to your variables
-  // since the context is changing, we use `port` to refer to current port to scan
-  let port = start
+const scanPorts = (host, start, end, timeout) => {
+  while (start <= end) {
+    // it is always good to give meaningful names to your variables
+    // since the context is changing, we use `port` to refer to current port to scan
+    let port = start
 
-  // we create an anonynous function, pass the current port, and operate on it
-  // the reason we encapsulate the socket creation process is because we want to preserve the value of `port` for the callbacks
-  ;(function(port) {
-    // console.log('CHECK: ' + port);
-    const socket = new net.Socket()
+    // we create an anonynous function, pass the current port, and operate on it
+    // the reason we encapsulate the socket creation process is because we want to preseve the value of `port` for the callbacks
+    ;(function(port) {
+      // console.log('CHECK: ' + port);
+      let socket = new net.Socket()
 
-    socket.setTimeout(timeout, function() {
-      socket.destroy()
-    })
-    socket.connect(port, host, function() {
-      console.log('OPEN: ' + port)
-      // we don't destroy the socket cos we want to listen to data event
-      // the socket will self-destruct in 2 secs cos of the timeout we set, so no worries
-    })
+      socket.setTimeout(timeout, function() {
+        socket.destroy()
+      })
+      socket.connect(port, host, function() {
+        console.log('OPEN: ' + port)
+        return `OPEN : ${port}`
+        // we don't destroy the socket cos we want to listen to data event
+        // the socket will self-destruct in 2 secs cos of the timeout we set, so no worries
+      })
 
-    // if any data is written to the client on connection, show it
-    socket.on('data', function(data) {
-      console.log(port + ': ' + data)
-      socket.destroy()
-    })
+      // if any data is written to the client on connection, show it
+      socket.on('data', function(data) {
+        console.log(port + ': ' + data)
+        socket.destroy()
+        return port + ': ' + data
+      })
 
-    socket.on('error', function(error) {
-      // silently catch all errors - assume the port is closed
-      socket.destroy()
-    })
-  })(port)
+      socket.on('error', function(error) {
+        // silently catch all errors - assume the port is closed
+        console.log(error)
+        socket.destroy()
+      })
+    })(port)
 
-  start++
+    start++
+  }
 }
+module.exports = scanPorts
